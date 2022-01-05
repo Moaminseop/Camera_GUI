04206064fa1b7b1bd3d0f9b1856ef06d198c905c
min seop song <sms971113@gmail.com>
2022-01-06 02:18:31 +0900

    1/6

--- ui_main.py
+++ ui_main.py
@@ -1,4 +1,5 @@
 import sys, os
+import cv2, imutils
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
@@ -32,22 +33,51 @@ class MyApp(QWidget):
         super().__init__()
         self.setAcceptDrops(True)
         self.titleBar()
-        self.content()
+        self.imageContent()
+        self.interfaceContent()
         self.initUI()
 
-    def mousePressEvent(self, event): 
-        self.offset = event.pos() 
+        self.filename = None
+        self.tmp = None
+
+    def loadImage(self):
+        self.filename = QFileDialog.getOpenFileName(filter="Image (*.*) ")[0]
+        self.image = cv2.imread(self.filename)
+        self.setPhoto(self.image)
+
+    def setPhoto(self, image):
+        self.tmp = image
+        image = imutils.resize(image, width = 750)
+        frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
+        self.imageLable.setPixmap(QtGui.QPixmap.fromImage(image))
+
+#     def mousePressEvent(self, event): 
+#         self.offset = event.pos() 
     
-    def mouseMoveEvent(self, event): 
-        x = event.globalX() 
-        y = event.globalY() 
-        x_w = self.offset.x() 
-        y_w = self.offset.y() 
-        self.move(x-x_w, y-y_w)
+#     def mouseMoveEvent(self, event): 
+#         x = event.globalX() 
+#         y = event.globalY() 
+#         x_w = self.offset.x() 
+#         y_w = self.offset.y() 
+#         self.move(x-x_w, y-y_w)
+    def mousePressEvent(self, event):
+        if event.button()==Qt.LeftButton:
+            self.m_flag=True
+            self.m_Position=event.globalPos()-self.pos() #Get the position of the mouse relative to the window
+            event.accept()
+            
+    def mouseMoveEvent(self, QMouseEvent):
+        if Qt.LeftButton and self.m_flag:  
+            self.move(QMouseEvent.globalPos()-self.m_Position)#Change window position
+            QMouseEvent.accept()
+            
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_flag=False
 
     def initUI(self):
         self.setWindowTitle('Camver')
-        self.resize(1280, 720)
+        self.resize(1180, 900)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setStyleSheet("""
                             background-color : rgb(40, 44, 52);
@@ -120,15 +150,15 @@ class MyApp(QWidget):
 
         self.btn_max.setCheckable(True)
         
-        self.titleBox = QFrame(self)
-        self.titleBox.setGeometry(0, 0, 1280, 50)
-        self.titleBox.setStyleSheet(
+        self.titleFrame = QFrame(self)
+        self.titleFrame.setGeometry(0, 0, 1180, 50)
+        self.titleFrame.setStyleSheet(
             ''' 
             QWidget{ 
                 background-color : rgb(33, 37, 43); }
             ''')
         
-        self.titleHBox= QHBoxLayout(self.titleBox) 
+        self.titleHBox= QHBoxLayout(self.titleFrame) 
         self.titleHBox.addWidget(self.favicon_label)
         self.titleHBox.addWidget(self.app_label)
         self.titleHBox.addStretch(1)
@@ -136,13 +166,13 @@ class MyApp(QWidget):
         self.titleHBox.addWidget(self.btn_max)
         self.titleHBox.addWidget(self.btn_close)
 
-    def content(self):
+    def imageContent(self):
         self.setAcceptDrops(True)
         self.imageLable = QLabel(self)
         self.imageLable.setAlignment(Qt.AlignCenter)
         self.imageLable.setText("Drop Image Here")
         self.imageLable.setFont(QFont('Segoe UI', 18))
-        self.imageLable.setGeometry(15, 65, 640, 640)
+        self.imageLable.setGeometry(70, 65, 750, 750)
         self.imageLable.setStyleSheet(
             """
             QLabel{
@@ -153,20 +183,104 @@ class MyApp(QWidget):
             }
             """
         )
+
+        self.fileOpenBtn = QPushButton('SELECT IMAGE', self)
+        self.fileOpenBtn.setFont(QFont('Segoe UI', 12))
+        self.fileOpenBtn.clicked.connect(self.loadImage)
+        #self.fileOpenBtn.setGeometry(820, 365, 150, 38)
+        #self.fileOpenBtn.setFixedSize(QtCore.QSize(150, 38))
+        self.fileOpenBtn.setMaximumWidth(250)
+        self.fileOpenBtn.setStyleSheet(
+            """
+            QPushButton{
+                background-color : rgba(20, 25, 30, 0.5); 
+                color : white;
+                margin-top : 5px;
+                padding : 7px 20px;
+                border : 0px;
+                border-radius : 5px;
+            }
+            
+            QPushButton::hover{ 
+                background-color : rgba(20, 25, 30, 1); 
+            }
+            """
+        )
         
 
-        # self.contentBox = QFrame(self)
-        # self.contentBox.setGeometry(0, 50, 1280, 670)
-        # self.contentBox.setStyleSheet(
-        #     ''' 
-        #     QWidget{ 
-
-        #     }
-        #     ''')
+        self.contentFrame = QFrame(self)
+        self.contentFrame.setGeometry(70, 65, 800, 800)
         
-        self.contentHBox = QHBoxLayout()
-        self.contentHBox.addWidget(self.imageLable)
-        #self.contentHBox.addStretch(1)
+        self.contentVBox = QVBoxLayout(self.contentFrame)
+        self.contentVBox.addWidget(self.imageLable)
+        self.contentVBox.addWidget(self.fileOpenBtn, alignment=QtCore.Qt.AlignHCenter)
+        #self.contentVBox.setAlignment(Qt.AlignCenter)
+
+
+    def interfaceContent(self):
+        self.resolutionCBox = QCheckBox('Resolution', self)
+        self.resolutionCBox.setFont(QFont('Segoe UI', 12))
+        self.resolutionCBox.setStyleSheet(
+            """
+            QCheckBox{
+                width : 30px;
+                height : 30px;
+                font-size : 22px;
+            }
+            """
+        )
+
+        self.vignettingCBox = QCheckBox('Vignetting', self)
+        self.vignettingCBox.setFont(QFont('Segoe UI', 12))
+        self.vignettingCBox.setStyleSheet(
+            """
+            QCheckBox{
+                width : 30px;
+                height : 30px;
+                font-size : 22px;
+            }
+            """
+        )
+
+        self.whiteBalanceCBox = QCheckBox('WhiteBalance', self)
+        self.whiteBalanceCBox.setFont(QFont('Segoe UI', 12))
+        self.whiteBalanceCBox.setStyleSheet(
+            """
+            QCheckBox{
+                width : 30px;
+                height : 30px;
+                font-size : 22px;
+            }
+            """
+        )
+
+        self.focusCBox = QCheckBox('Focus', self)
+        self.focusCBox.setFont(QFont('Segoe UI', 12))
+        self.focusCBox.setStyleSheet(
+            """
+            QCheckBox{
+                width : 30px;
+                height : 30px;
+                font-size : 22px;
+            }
+            """
+        )
+
+
+        self.analyzerLabel = QLabel(self)
+        self.analyzerLabel.setText("Analyzer")
+        self.analyzerLabel.setFont(QFont('Segoe UI', 30))
+
+        self.interfaceFrame = QFrame(self)
+        self.interfaceFrame.setGeometry(900, 65, 200, 500)
+
+        self.interfaceContentVBox = QVBoxLayout(self.interfaceFrame)
+        self.interfaceContentVBox.addWidget(self.analyzerLabel)
+        self.interfaceContentVBox.addWidget(self.resolutionCBox)
+        self.interfaceContentVBox.addWidget(self.vignettingCBox)
+        self.interfaceContentVBox.addWidget(self.whiteBalanceCBox)
+        self.interfaceContentVBox.addWidget(self.focusCBox)
+        
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasImage:
